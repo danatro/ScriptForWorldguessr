56961b58990bc32d1cde2165a0eb631ac6e5c55d
danatro <46900311+danatro@users.noreply.github.com>
2025-01-07 20:59:12 +0100

    Update

--- ChangeBackground2Gif.js
+++ ChangeBackground2Gif.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name         Change Background for WorldGuessr
+// @name         Change Animated Background for WorldGuessr
+// @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Change the game background image on WorldGuessr
+// @description  Change the game background to an animated one on WorldGuessr
 // @author       DanaBZH, Danatro
 // @match        https://www.worldguessr.com/*
 // ==/UserScript==
@@ -9,25 +10,31 @@
 (function() {
     'use strict';
 
-    // Replace this URL with the link to your desired background image
-    const newBackgroundUrl = "https://wallpaper.forfun.com/fetch/87/87c93aa33275b4c8c73637ad3fbee836.jpeg";
+    // === Configuration ===
+    // Replace this URL with the link to your desired animated background (GIF or video)
+    const animatedBackgroundUrl = "https://github.com/danatro/ScriptForWorldguessr/blob/main/gifbackground.gif?raw=true";
 
 
-    function changeBackground() {
+    function changeBackgroundToAnimated() {
 
         const xpath = "/html/body/div/div[5]/img";
         const imgElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
         if (imgElement) {
 
-            imgElement.src = newBackgroundUrl;
+            imgElement.src = animatedBackgroundUrl;
 
-            console.log("Background image changed to:", newBackgroundUrl);
+
+            imgElement.style.objectFit = "cover";
+            imgElement.style.width = "100%";
+            imgElement.style.height = "100%";
+
+            console.log("Background changed to animated:", animatedBackgroundUrl);
         } else {
             console.warn("Could not find the background image element.");
         }
     }
 
 
-    window.addEventListener('load', changeBackground);
+    window.addEventListener('load', changeBackgroundToAnimated);
 })();
